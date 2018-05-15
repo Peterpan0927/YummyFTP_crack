c4e9a16276f2059ec1d9b50f1afeb5f4cd664139
Peterpan0927 <1549346936@qq.com>
2018-05-15 22:03:31 +0800

    增加自动化脚本并进行详细描述

---
+++ copy.sh
@@ -0,0 +1,5 @@
+!# /bin/bash
+mv /Application/Yummy\ FTP.app/Contents/MacOS/Yummy\ FTP ~/Yummy_FTP.cp
+echo "your original copy file is at ~/Yummy_FTP.cp"
+cp ./Yummy\ FTP /Application/Yummy\ FTP.app/Contents/MacOS/Yummy\ FTP
+echo "Now you can use it without buying it ,having fun~"
